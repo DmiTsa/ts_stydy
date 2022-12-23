c742f672969aafae5bb6a08bdfe866feb82ad929
DmiTsa <you@example.com>
2022-12-23 10:48:08 +0300

    1

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,26 @@
-// import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
 import getController from './controllers/getController';
 
 const port = 4000;
-console.log(getController);
+// console.log(createServer, IncomingMessage, ServerResponse);
 
-// const server = createServer(
-//   (request: IncomingMessage, response: ServerResponse) => {
-//     switch (request.method) {
-//       case 'GET':
-//         getController(request, response);
-//         break;
-//       default:
-//         response.statusCode = 500;
-//         response.write(
-//           `Error 500: request type ${request.method} is not supported`
-//         );
-//         response.end();
-//         break;
-//     }
-//   }
-// );
+const server = createServer(
+  (request: IncomingMessage, response: ServerResponse) => {
+    switch (request.method) {
+      case 'GET':
+        getController(request, response);
+        break;
+      default:
+        response.statusCode = 500;
+        response.write(
+          `Error 500: request type ${request.method} is not supported`
+        );
+        response.end();
+        break;
+    }
+  }
+);
+
+server.listen(port, function () {
+  console.log(`Server running at port: ${port}`);
+});
